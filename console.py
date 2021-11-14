667a097f9a1984045c88676e70b2fcce0e35dcd3
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-11-14 21:48:23 +0000

    Console, handling EOF, quit, and Empty line

--- console.py
+++ console.py
@@ -1,1 +1,29 @@
-command interpreter
+#!/usr/bin/python3
+"""
+Contains the entry point of the command interpreter for HBnB
+"""
+
+import cmd
+
+
+class HBNBCommand(cmd.Cmd):
+    """
+    Define HBnB console
+    """
+
+    prompt = "(hbnb) "
+
+    def do_quit(self, arg):
+        """Quit command to exit console"""
+        return True
+
+    def do_EOF(self, arg):
+        """EOF signal to exit console"""
+        return True
+
+    def emptyline(self):
+        """overwriting the emptyline method"""
+        return False
+
+if __name__ == '__main__':
+    HBNBCommand().cmdloop()
