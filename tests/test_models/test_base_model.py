8307c3b88a8493c2a0bec4d5f57fa25a6bd80496
johanne101 <jlopez0493@gmail.com>
2021-11-16 04:20:02 +0000

    [save]

--- tests/test_models/test_base_model.py
+++ tests/test_models/test_base_model.py
@@ -11,62 +11,64 @@ class TestBase_Model(unittest.TestCase):
     """Test for BaseModel class methods
     """
 
-    def test_instance(self):
+    def test_inst(self):
         inst = BaseModel()
 
     def setUpClass(self):
-        ''' Set two base models'''
-        self.test0= BaseModel()
+        ''' Test Basic base models'''
+        self.base = BaseModel()
         self.base_model_ = BaseModel()
 
     def test_str(self):
         ''' Test __str__  '''
-         self.assertIsInstance(self.test0.__str__(), str)
+        base = BaseModel()
+        base_model = "[BaseModel]"
+        self.assertIsInstance(self.base.id, self.test0.__str__(), str)
 
     def test_str_content(self):
-        """checks content of __str__"""
-        self.assertIn("[BaseModel]", self.B_base_model.__str__())
+        """verify if __str__ content true"""
+        self.assertIn("[BaseModel]", self.base_model.__str__())
 
     def test_Init(self):
-        ''' Checks if Init works'''
-        self.assertIsInstance(self.A_base_model, BaseModel)
-        self.assertIsInstance(self.B_base_model, BaseModel)
+        ''' Test Init'''
+        self.assertIsInstance(self.base, BaseModel)
+        self.assertIsInstance(self.base_model, BaseModel)
 
     def test_Id(self):
-        ''' Test if each instance have an unique id '''
-        self.assertNotEqual(self.A_base_model.id,
-                            self.B_base_model.id)
+        ''' Test for unique id '''
+        self.assertNotEqual(self.base.id,
+                            self.base_model.id)
 
     def test_id_str(self):
-        '''test if id is str'''
-        self.assertIsInstance(self.A_base_model.id, str)
+        '''test if id is an str'''
+        self.assertIsInstance(self.base.id, str)
 
-    def test_date_is_date(self):
-        '''test if dates are datetime type'''
-        self.assertIsInstance(self.A_base_model.created_at, datetime)
-        self.assertIsInstance(self.A_base_model.updated_at, datetime)
+    def test_d_is_equal(self):
+        '''test datetime'''
+        self.assertIsInstance(self.base.created_at, datetime)
+        self.assertIsInstance(self.base.updated_at, datetime)
 
-    def test_date_update(self):
-        '''test if datetimes updates correctly'''
-        self.A_base_model.save()
-        self.assertNotEqual(self.A_base_model.created_at,
-                            self.A_base_model.updated_at)
+    def test_d_update(self):
+        '''test if saved is equal'''
+        self.base.save()
+        self.assertNotEqual(self.base.created_at,
+                            self.base.updated_at)
 
     def test_to_dict(self):
         """test if to_dict returns a dictionary"""
-        self.assertIsInstance(self.A_base_model.to_dict(), dict)
+        self.assertIsInstance(self.base.to_dict(), dict)
 
     def test_to_dict_has_key(self):
         """test if to_dict works"""
-        self.assertIn("created_at", self.A_base_model.to_dict())
+        self.assertIn("created_at", self.base.to_dict())
 
     def test_add_attribute(self):
         """test adding attribute"""
-        self.A_base_model.name = "pepe"
-        self.assertEqual(self.A_base_model.name, "pepe")
+        self.base.name = "name"
+        self.assertEqual(self.base.name, "name")
 
     def test_kwarg(self):
-        objdict = self.A_base_model.to_dict()
+        objdict = self.base.to_dict()
         self.kwarginstance = BaseModel(**objdict)
         self.assertIsInstance(self.kwarginstance, BaseModel)
 
