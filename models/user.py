9f005aaa65341ea2f2274ea4f92e8e64116c0ef0
Gabriela Martinez <g.martinez.rodz@gmail.com>
2021-11-17 01:59:34 +0000

    Uploading class user

--- models/user.py
+++ models/user.py
@@ -1,1 +1,16 @@
 #!/usr/bin/python3
+""" Class User"""
+
+from models.base_model import BaseModel
+
+
+class User(BaseModel):
+    """Representation of a user """
+    email = ""
+    password = ""
+    first_name = ""
+    last_name = ""
+
+    def __init__(self, *args, **kwargs):
+        """initializes user"""
+        super().__init__(*args, **kwargs)
