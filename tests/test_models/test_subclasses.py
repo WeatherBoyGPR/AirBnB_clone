8ed64be559b883b97d809c7bac5250225508f5fb
WeatherBoyGPR <gp.rossijr@gmail.com>
2022-02-08 20:23:04 +0000

    added tests for User class

---
+++ tests/test_models/test_subclasses.py
@@ -0,0 +1,126 @@
+#!/usr/bin/python3
+"""Test ModulePackage:
+    Unittests for classes below:
+    User
+    State
+    City
+    Amenity
+    Place
+    Review
+"""
+
+import os
+import models
+import unittest
+import datetime
+import uuid
+from models.base_model import BaseModel
+from models.user import User
+from models.engine.file_storage import FileStorage
+
+
+class TestBase_Model(unittest.TestCase):
+    """
+        Test for BaseModel class methods
+    """
+
+    def test_inst(self):
+        inst = BaseModel()
+
+    @classmethod
+    def setUpClass(self):
+        ''' Test Basic base models'''
+        self.base = BaseModel()
+        self.base_model = BaseModel()
+
+    def test_str(self):
+        ''' Test __str__  '''
+        base = BaseModel()
+        base_model = "[BaseModel]"
+        self.assertIsInstance
+        (self.base.__str__(), str)
+
+    def test_str_content(self):
+        """verify if __str__ content true"""
+        self.assertIn("[BaseModel]", self.base_model.__str__())
+
+    def test_Init(self):
+        ''' Test Init'''
+        self.assertIsInstance(self.base, BaseModel)
+        self.assertIsInstance(self.base_model, BaseModel)
+
+    def test_Id(self):
+        ''' Test for unique id '''
+        self.assertNotEqual(self.base.id, self.base_model.id)
+
+    def test_id_str(self):
+        '''test if id is an str'''
+        self.assertIsInstance(self.base.id, str)
+
+    def test_d_is_equal(self):
+        '''test datetime'''
+        self.assertIsInstance(self.base.created_at, datetime.datetime)
+        self.assertIsInstance(self.base.updated_at, datetime.datetime)
+
+    def test_d_update(self):
+        '''test if saved is equal'''
+        self.base.save()
+        self.assertNotEqual(self.base.created_at, self.base.updated_at)
+
+        def test_to_dict(self):
+            """test if to_dict returns a dictionary"""
+        self.assertIsInstance(self.base.to_dict(), dict)
+
+    def test_to_dict_has_key(self):
+        """test if to_dict works"""
+        self.assertIn("created_at", self.base.to_dict())
+
+    def test_add_attribute(self):
+        """test adding attribute"""
+        self.base.name = "name"
+        self.assertEqual(self.base.name, "name")
+
+    def test_kwarg(self):
+        objdict = self.base.to_dict()
+        self.kwarginstance = BaseModel(**objdict)
+        self.assertIsInstance(self.kwarginstance, BaseModel)
+
+
+class TestUser(unittest.TestCase):
+    """
+        Test for User class methods
+    """
+
+    def test_Init(self):
+        """ Test Init """
+        Inst = User()
+        self.assertIsInstance(Inst, User)
+
+    @classmethod
+    def setUpClass(self):
+        """ Test User class """
+        self.testUser = User()
+
+    def test_email(self):
+        """ Test email attribute """
+        self.testUser.email = "1234@goog.meep"
+        self.assertIsInstance(self.testUser.email, str)
+
+    def test_password(self):
+        """ Test password attribute """
+        self.testUser.password = "1234"
+        self.assertIsInstance(self.testUser.password, str)
+
+    def test_first_name(self):
+        """ Test first_name attribute """
+        self.testUser.first_name = "1234"
+        self.assertIsInstance(self.testUser.first_name, str)
+
+    def test_last_name(self):
+        """ Test last_name attribute """
+        self.testUser.last_name = "1234"
+        self.assertIsInstance(self.testUser.last_name, str)
+
+
+if __name__ == "__main__":
+    unittest.main()
